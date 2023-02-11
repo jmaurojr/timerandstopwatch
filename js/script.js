b88234cbb88b6def934bf9d09f748109cb6929c7
jmaurojr <112908979+jmaurojr@users.noreply.github.com>
2023-02-10 21:02:12 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,39 @@
+document.getElementById("temaAzul").addEventListener("click", function () {
+  document.querySelector("div.sqr1").setAttribute("class", "sqr1bl");
+});
+
+document.getElementById("temaAzul").addEventListener("click", function () {
+  document.querySelector("div.sqr2").setAttribute("class", "sqr2bl");
+});
+
+document.getElementById("temaAzul").addEventListener("click", function () {
+  document.querySelector("div.sqr3").setAttribute("class", "sqr3bl");
+});
+
+document.getElementById("temaAzul").addEventListener("click", function () {
+  document.querySelector("div.sqr4").setAttribute("class", "sqr4bl");
+});
+
+document.getElementById("temaAzul").addEventListener("click", function () {
+  document.querySelector("div.sqr5").setAttribute("class", "sqr5bl");
+});
+
+document.getElementById("temaVerde").addEventListener("click", function () {
+  document.querySelector("div.sqr1bl").setAttribute("class", "sqr1");
+});
+
+document.getElementById("temaVerde").addEventListener("click", function () {
+  document.querySelector("div.sqr2bl").setAttribute("class", "sqr2");
+});
+
+document.getElementById("temaVerde").addEventListener("click", function () {
+  document.querySelector("div.sqr3bl").setAttribute("class", "sqr3");
+});
+
+document.getElementById("temaVerde").addEventListener("click", function () {
+  document.querySelector("div.sqr4bl").setAttribute("class", "sqr4");
+});
+
+document.getElementById("temaVerde").addEventListener("click", function () {
+  document.querySelector("div.sqr5bl").setAttribute("class", "sqr5");
+});
---
+++ readme.md
@@ -0,0 +1,19 @@
+[] - Timer
+[] - StopWatch
+[] - Preferences
+[] - keyboard shortcuts and icon
+[] - 5min, 10min increase/decrease buttons
+
+/_Verde_/
+cad2c5
+84a98c
+52796f
+354f52
+2f3e46
+
+/_Azul_/
+e0e1dd
+778da9
+415a77
+1b263b
+0d1b2a
